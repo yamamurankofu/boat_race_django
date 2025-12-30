77e5c7e0cc45d5bc2a093287d935f7004bce6bd6
github yamamurankofu <yamamurankofu@gmail.com>
2025-12-31 01:40:59 +0900

    Fix: Remove STATICFILES_DIRS and duplicate STATIC_URL

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ CSRF_TRUSTED_ORIGINS = ['http://localhost:8000']
 
 # 静的ファイルの設定
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 
@@ -118,7 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
